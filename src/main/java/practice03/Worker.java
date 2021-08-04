ae609353eee46702e9ca1d52f937b29c3c7f2824
janchua19 <jan.edward.chua@oocl.com>
2021-08-04 16:31:37 +0800

    [Practice03][JanEdward] feat: implement should_worker_have_name_and_age

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,19 @@
 package practice03;
 
 public class Worker {
+    private String name;
+    private int age;
+
+    public Worker(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
