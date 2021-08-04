efd060423ee1225bbcda308ed0d372c1f3aee218
janchua19 <jan.edward.chua@oocl.com>
2021-08-04 18:03:01 +0800

    [Practice05][JanEdward] feat: should_person_have_an_introduce_method_which_introduce_person_with_name_and_age

--- src/main/java/practice05/Person.java
+++ src/main/java/practice05/Person.java
@@ -17,6 +17,6 @@ public class Person {
         return age;
     }
     public String introduce(){
-        return "My name is " + name + ". I am " + age + "years old.";
+        return "My name is " + name + ". I am " + age + " years old.";
     }
 }
