94678d498099daaf7f0b8aaa69674278b3b9cac4
janchua19 <jan.edward.chua@oocl.com>
2021-08-04 22:45:02 +0800

    [P8][Jan] feat: implement should_person_have_id_name_and_age

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,13 @@
 package practice08;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,13 @@
 package practice08;
 
-public class Student {
+public class Student extends Person{
+    Klass klass;
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public String introduce(){
+        String message = super.introduce() + " I am a Student.";
+        return message + " I am at Class " + klass.getNumber() + ".";
+    }
 }
