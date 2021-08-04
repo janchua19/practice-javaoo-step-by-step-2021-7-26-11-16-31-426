84ea2de8d1c0b1ee5936c65cea0cb31656b26400
janchua19 <jan.edward.chua@oocl.com>
2021-08-04 17:04:49 +0800

    [Practice04][JanEdward[ feat: implement should_student_introduce_with_class

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -3,14 +3,13 @@ package practice04;
 public class Student extends Person{
 
     private int klass;
-    private String name;
-    private int age;
+
     public Student(String name, int age, int klass) {
         super(name, age);
         this.klass = klass;
     }
     public String introduce(){
-        return "My name is " + name + ". I am " + age + " years old. I am a Student. I am at Class " + klass + ".";
+        return "My name is " + super.getName() + ". I am " + super.getAge() + " years old. I am a Student. I am at Class " + klass + ".";
     }
 
     public int getKlass() {
