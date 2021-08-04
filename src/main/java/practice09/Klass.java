1ed2cc9c356b52dacc08fe2686ad6139fafeb621
janchua19 <jan.edward.chua@oocl.com>
2021-08-05 00:21:16 +0800

    [Prac9][Jan] feat: implement should_person_have_id_name_and_age

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,18 @@
 package practice09;
 
 public class Klass {
+    private Student leader;
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
