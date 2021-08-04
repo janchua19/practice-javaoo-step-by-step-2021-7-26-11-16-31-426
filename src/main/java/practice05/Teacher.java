316ccd8f0d3d171c296e536433e98fae91665142
janchua19 <jan.edward.chua@oocl.com>
2021-08-04 18:20:49 +0800

    [Practice05][JanEdward] feat: implement should_teacher_have_name_and_age_and_class()

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,25 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+    private int nullKlass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    private String message = "My name is " + super.getName() + ". I am " + super.getAge() + " years old. I am a Teacher.";
+
+    public String introduce(){
+        if(klass == nullKlass)
+        {
+            return message + " I teach No Class.";
+        }
+            return message + " I teach Class "+klass+".";
+    }
 }
