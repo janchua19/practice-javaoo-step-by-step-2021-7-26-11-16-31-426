1360484856a708ce926d380c08160be95d6d97dc
janchua19 <jan.edward.chua@oocl.com>
2021-08-06 23:32:45 +0800

    [Practice10][Jan] feat: implements should_teacher_introduce_itself_with_which_classes_it_teaches

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -29,7 +29,19 @@ public class Teacher extends Person{
     public String introduce() {
         String message = super.introduce() + " I am a Teacher.";
 
+        if(klasses != null) {
+            message += " I teach Class ";
 
+            for(Klass klass : klasses)
+            {
+                int i;
+                message += klass.getNumber();
+                if(klass != klasses.getLast()){
+                    message += ", ";
+                }
+            }
+            return message += ".";
+        }
         return message;
     }
 }
