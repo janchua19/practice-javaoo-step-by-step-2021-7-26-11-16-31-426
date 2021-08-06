bb84391a18dd9fdbbd089b9fdd1d2d73cf0bd366
janchua19 <jan.edward.chua@oocl.com>
2021-08-06 23:01:51 +0800

    [Practice10][Jan] test: create should_teacher_have_name_and_age_and_classes

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,23 @@
 package practice10;
 
-public class Teacher {
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    public Teacher(int id, String name, int age, LinkedList<Klass> klasses) {
+        super(id, name, age);
+
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public int getAge() {
+        return 0;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return null;
+    }
 }
