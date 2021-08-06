6bde0df1a8202f8d76628e5caa66a220569ed29b
janchua19 <jan.edward.chua@oocl.com>
2021-08-06 22:34:59 +0800

    [Practice10][Jan] test: create should_student_have_name_age_and_class_number

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,7 +1,21 @@
 package practice10;
 
 public class Student {
+    Klass klass;
+
     public Student(int id, String name, int age, Klass klass) {
 
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public int getAge() {
+        return 0;
+    }
+
+    public Klass getKlass() {
+        return klass;
     }
 }
